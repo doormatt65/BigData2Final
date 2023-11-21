46644dcda588f3d8ab071bc8226e05e1a548dabe
Matt Braun <mbraun3@emich.edu>
2023-11-21 10:51:37 -0500

    better logging on upload

--- batchupload.sh
+++ batchupload.sh
@@ -2,10 +2,10 @@
 
 AWS_PROFILE="default"
 AWS_REGION="us-east-2"
-DYNAMODB_TABLE_NAME="BookData"
+DYNAMODB_TABLE_NAME="Books"
 
 request_items=""
-batch_size=10  # Adjust this batch size according to your needs
+batch_size=25  # Adjust this batch size according to your needs
 
 process_batch() {
   local items=$1
@@ -15,11 +15,14 @@ process_batch() {
 }
 
 counter=0
+skipped_items=0
+processed_items=0
 
 while IFS=, read -r bookId Title Authors AverageRating ISBN NumPages Publisher
 do
   if [[ ! $AverageRating =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
-    echo "Invalid AverageRating value: $AverageRating"
+    echo "Skipping invalid item: $bookId - $Title (Invalid AverageRating: $AverageRating)"
+    ((skipped_items++))
     continue
   fi
 
@@ -29,6 +32,7 @@ do
   request_items+="{\"PutRequest\":{\"Item\":{\"bookId\":{\"S\":\"$bookId\"},\"Title\":{\"S\":\"$EscapedTitle\"},\"Authors\":{\"S\":\"$Authors\"},\"AverageRating\":{\"N\":\"$AverageRating\"},\"ISBN\":{\"S\":\"$ISBN\"},\"NumPages\":{\"N\":\"$NumPages\"},\"Publisher\":{\"S\":\"$EscapedPublisher\"}}}},"
 
   ((counter++))
+  ((processed_items++))
 
   if [ $counter -eq $batch_size ]; then
     # Process the batch
@@ -41,4 +45,8 @@ done < bookdata.csv
 # Process the remaining items if not a complete batch
 if [ ${#request_items} -gt 0 ]; then
   process_batch "${request_items%,}"
+  ((processed_items+=$((counter - 1))))
 fi
+
+echo "Total processed items: $processed_items"
+echo "Total skipped items: $skipped_items"
